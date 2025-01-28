746e02c6c376e6b24e95305a29ed90dcf81d13f5
furqonstudio <furqonstudio@gmail.com>
2025-01-28 08:30:49 +0700

    feat: disable auth temporarily

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -10,10 +10,16 @@ const authenticateToken = require('../middlewares/authMiddleware')
 
 const router = express.Router()
 
-router.get('/', authenticateToken, getEmployees)
-router.get('/:id', authenticateToken, getEmployeeById)
-router.post('/', authenticateToken, createEmployee)
-router.put('/:id', authenticateToken, updateEmployee)
-router.delete('/:id', authenticateToken, deleteEmployee)
+// router.get('/', authenticateToken, getEmployees)
+// router.get('/:id', authenticateToken, getEmployeeById)
+// router.post('/', authenticateToken, createEmployee)
+// router.put('/:id', authenticateToken, updateEmployee)
+// router.delete('/:id', authenticateToken, deleteEmployee)
+
+router.get('/', getEmployees)
+router.get('/:id', getEmployeeById)
+router.post('/', createEmployee)
+router.put('/:id', updateEmployee)
+router.delete('/:id', deleteEmployee)
 
 module.exports = router
--- src/routes/officeRoutes.js
+++ src/routes/officeRoutes.js
@@ -10,10 +10,16 @@ const authenticateToken = require('../middlewares/authMiddleware')
 
 const router = express.Router()
 
-router.get('/', authenticateToken, getOffices)
-router.get('/:id', authenticateToken, getOfficeById)
-router.post('/', authenticateToken, createOffice)
-router.put('/:id', authenticateToken, updateOffice)
-router.delete('/:id', authenticateToken, deleteOffice)
+// router.get('/', authenticateToken, getOffices)
+// router.get('/:id', authenticateToken, getOfficeById)
+// router.post('/', authenticateToken, createOffice)
+// router.put('/:id', authenticateToken, updateOffice)
+// router.delete('/:id', authenticateToken, deleteOffice)
+
+router.get('/', getOffices)
+router.get('/:id', getOfficeById)
+router.post('/', createOffice)
+router.put('/:id', updateOffice)
+router.delete('/:id', deleteOffice)
 
 module.exports = router
